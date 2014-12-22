47c54b1604445acf3cd4a0193abb290f30a26664
Matt Morris <matt.morris@placewise.com>
2014-12-21 19:58:06 -0700

    monkey patch Aray for calculating diagonals src: https://gist.github.com/EvilScott/1755729

--- 11.rb
+++ 11.rb
@@ -26,6 +26,22 @@
 
 # What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the 20×20 grid?
 
+class Array
+  def diagonals
+    [self, self.map(&:reverse)].inject([]) do |all_diags, matrix|
+      ((-matrix.count + 1)..matrix.first.count).each do |offet_index|
+        diagonal = []
+        (matrix.count).times do |row_index|
+          col_index = offet_index + row_index
+          diagonal << matrix[row_index][col_index] if col_index >= 0
+        end
+        all_diags << diagonal.compact if diagonal.compact.count > 1
+      end
+      all_diags
+    end
+  end
+end
+
 grid = [[ 8, 2,22,97,38,15, 0,40, 0,75, 4, 5, 7,78,52,12,50,77,91, 8],
         [49,49,99,40,17,81,18,57,60,87,17,40,98,43,69,48, 4,56,62, 0],
         [81,49,31,73,55,79,14,29,93,71,40,67,53,88,30, 3,49,13,36,65],
@@ -46,3 +62,6 @@ grid = [[ 8, 2,22,97,38,15, 0,40, 0,75, 4, 5, 7,78,52,12,50,77,91, 8],
         [20,69,36,41,72,30,23,88,34,62,99,69,82,67,59,85,74, 4,36,16],
         [20,73,35,29,78,31,90, 1,74,31,49,71,48,86,81,16,23,57, 5,54],
         [ 1,70,54,71,83,51,54,69,16,92,33,48,61,43,52, 1,89,19,67,48]]
+
+require 'pry'
+binding.pry
